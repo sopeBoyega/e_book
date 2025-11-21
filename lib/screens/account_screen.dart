29e66b7e51706eef8bb407768f95119522bd890d
Favour Ikechukwu <149949474+Favour-Ikec@users.noreply.github.com>
2025-11-21 12:05:49 +0100

    Made changes to book.dart

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -6,7 +6,7 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Account screen'),
+      child: Text('Account Screen'),
     );
   }
 }
